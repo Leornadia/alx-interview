85eda4c02001e05140fc80703a67382e17e3c0ae
Leornadia <leornadiajose@gmail.com>
2024-07-31 03:00:16 -0700

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,25 +4,19 @@
 def canUnlockAll(boxes):
     """
     Determines if all boxes can be unlocked.
-
-    Args:
-        boxes (list): A list of lists representing lockboxes and keys.
-
-    Returns:
-        bool: True if all boxes can be unlocked, False otherwise.
+    
+    :param boxes: list of lists representing the boxes and their keys
+    :return: True if all boxes can be unlocked, False otherwise
     """
+    n = len(boxes)
+    unlocked = [1] + [0] * (n - 1)
+    keys = [0]
 
-    num_boxes = len(boxes)
-    unlocked = [False] * num_boxes
-    unlocked[0] = True  
-    queue = [0]  
+    for key in keys:
+        for new_key in boxes[key]:
+            if new_key < n and not unlocked[new_key]:
+                unlocked[new_key] = 1
+                keys.append(new_key)
 
-    while queue:
-        current_box = queue.pop(0)
-        for key in boxes[current_box]:
-            if not unlocked[key] and key < num_boxes:
-                unlocked[key] = True
-                queue.append(key)
-
-    return all(unlocked)  
+    return sum(unlocked) == n
 
