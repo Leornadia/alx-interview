85afcf58f3803b035ea6296e053b0c538889b0a5
Leornadia <leornadiajose@gmail.com>
2024-07-31 02:47:41 -0700

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,22 +3,23 @@
 
 def canUnlockAll(boxes):
     """
-    Determines if all boxes can be unlocked.
+    Determines if all boxes can be unlocked using depth-first search.
     
     :param boxes: list of lists representing the boxes and their keys
     :return: True if all boxes can be unlocked, False otherwise
     """
-    if not boxes or len(boxes) == 0:
+    if not boxes:
         return False
-    
+
     n = len(boxes)
-    unlocked = [False] * n
-    unlocked[0] = True
-    keys = boxes[0]
-    
-    for key in keys:
-        if key < n and not unlocked[key]:
-            unlocked[key] = True
-            keys.extend(boxes[key])
-    
-    return all(unlocked)
+    visited = [False] * n
+    visited[0] = True
+
+    def dfs(box):
+        for key in boxes[box]:
+            if isinstance(key, int) and 0 <= key < n and not visited[key]:
+                visited[key] = True
+                dfs(key)
+
+    dfs(0)
+    return all(visited)
