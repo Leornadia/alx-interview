596a0ac9124e8f66fe161d096f19e914d27c13fc
Leornadia <leornadiajose@gmail.com>
2024-08-08 15:16:04 -0700

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,28 +6,17 @@ required to get 'n' characters using only "Copy All" and "Paste" operations.
 
 
 def minOperations(n):
-    """
-    Calculates the minimum number of operations required to get 'n' characters
-    using "Copy All" and "Paste" operations.
-
-    Args:
-        n: The target number of characters.
-
-    Returns:
-        The minimum number of operations required.
-        If n is impossible to achieve, return 0.
-    """
     if n <= 1:
         return 0
-
+    
     operations = 0
     factor = 2
-
+    
     while factor <= n:
         if n % factor == 0:
-            operations += 1
+            operations += factor
             n //= factor
         else:
             factor += 1
-
+    
     return operations
