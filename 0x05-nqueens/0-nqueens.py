1597b9425cf3a2acdb86983623ddefeb5939dc68
Leornadia <leornadiajose@gmail.com>
2024-08-29 02:06:44 -0700

    0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,35 +1,46 @@
 #!/usr/bin/python3
 import sys
 
-def is_safe(board, row, col):
-    # Check the same column
-    for i in range(row):
-        if board[i] == col:
+def is_safe(board, row, col, n):
+    # Check this row on left side
+    for i in range(col):
+        if board[row][i] == 1:
             return False
-    # Check the main diagonal
-    for i in range(row):
-        if board[i] - i == col - row:
+
+    # Check upper diagonal on left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
             return False
-    # Check the secondary diagonal
-    for i in range(row):
-        if board[i] + i == col + row:
+
+    # Check lower diagonal on left side
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
             return False
+
     return True
 
-def solve_nqueens(N, row, board, solutions):
-    if row == N:
-        solutions.append(board[:])
-        return
+def solve_nqueens(n):
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    solutions = []
 
-    for col in range(N):
-        if is_safe(board, row, col):
-            board[row] = col
-            solve_nqueens(N, row + 1, board, solutions)
-            board[row] = -1
+    def solve(col):
+        if col >= n:
+            solution = []
+            for i in range(n):
+                for j in range(n):
+                    if board[i][j] == 1:
+                        solution.append([i, j])
+            solutions.append(solution)
+            return True
 
-def print_solutions(solutions):
-    for solution in solutions:
-        print([[i, solution[i]] for i in range(len(solution))])
+        for i in range(n):
+            if is_safe(board, i, col, n):
+                board[i][col] = 1
+                solve(col + 1)
+                board[i][col] = 0
+
+    solve(0)
+    return solutions
 
 def main():
     if len(sys.argv) != 2:
@@ -37,20 +48,18 @@ def main():
         sys.exit(1)
 
     try:
-        N = int(sys.argv[1])
+        n = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
 
-    if N < 4:
+    if n < 4:
         print("N must be at least 4")
         sys.exit(1)
 
-    board = [-1] * N
-    solutions = []
-    solve_nqueens(N, 0, board, solutions)
-    print_solutions(solutions)
+    solutions = solve_nqueens(n)
+    for solution in solutions:
+        print(solution)
 
 if __name__ == "__main__":
     main()
-
